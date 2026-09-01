bb50350cf3a0f5a4161c696b8caf7e83979c590a
jduja <jduja681@gmail.com>
2026-09-01 22:17:58 +0800

    asd

--- PlanetEscape/PlanetEscape/SceneDelegate.swift
+++ PlanetEscape/PlanetEscape/SceneDelegate.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import AppTrackingTransparency
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -23,6 +24,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            ATTrackingManager.requestTrackingAuthorization { statue in
+                
+            }
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
